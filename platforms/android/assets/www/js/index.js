a4089e346df713f39918e1c02185e1774dd28fd8
reinis <reinis@192.168.2.102>
2015-10-05 22:45:39 +0300

    smth

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,27 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.initMap();
-    },
-    initMap: function () {
+function initMap () {
            
         // Define Bing api key.
         var strBingApiKey = 'AlUNqOceitHsnEtv7APDQZKuC_HOxQVLdhkvxP5MD0rknzeHvvZ0OtQMYlbxZ39O';
@@ -148,4 +125,3 @@ var app = {
         }
  
     }
-};
